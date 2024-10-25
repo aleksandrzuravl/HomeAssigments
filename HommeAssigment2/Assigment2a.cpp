8c165f07887a1e24e8ffd1c2d6d8b79e4a5bfaf8
alexz <st129760@student.spbu.ru>
2024-10-25 11:23:19 +0300

    home assigment 2a

---
+++ HommeAssigment2/Assigment2a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+int main()
+{
+    std::ifstream file("text.txt", std::ios_base::binary);
+    int size = std::filesystem::file_size('text.txt');
+    char *lst = new char[size];
+    file.read(lst, size);
+    
+    
+    
+    char x;
+    for(int i = 0; i <= size - size/2; i++){
+        x = lst[i];
+        lst[i]= lst[size - 1 - i];
+        lst[size - 1 - i] = x;
+     }
+     
+     
+     std::ofstream outfile;
+     std::outfile.open("temppdf.pdf", std::ios::binary|std::ios::out);
+     outfile.write((char*)&lst,sizeof(lst));
+     
+     
+     infile.close
+     outfile.close
+     
+     delete[lst]
+        
+        
+}
