d78c53d98ea99bc788eaa095e2489d145e84cd7a
alexz <st129760@student.spbu.ru>
2024-11-25 16:15:51 +0300

    cpp file func

---
+++ HommeAssigment1/hello.cpp
@@ -0,0 +1,17 @@
+/*zhuravlev alexander st129760
+    assigment 1, realization func
+*/
+#include <iostream>
+using namespace std;
+
+
+void func(){
+    cout << "Hello, world" << endl;
+    while (true){
+        string name = "ooo";
+        cin >> name ;
+        cout << "Hello, " << name << endl;
+     }
+     return;   
+} 
+
